b55c718d7e54a538e5701f3ffab505c5b46a673f
Muriithi Gakuru <antony123muriithi@gmail.com>
2022-10-09 23:48:18 +0300

    add color mode as variable

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -7,33 +7,34 @@ function SideBar({ darkMode }) {
         backgroundColor: darkMode ? '#f2f2f2' : 'black',
         color: darkMode ? 'black' : 'white'
     }
+    const colorMode = {color: darkMode ? 'black' : '#f2f2f2'}
     return (
         <div className="d-flex flex-column flex-shrink-0 p-3 d-none d-md-block" style={styles}>
             <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
             </a>
             <ul className="nav nav-pills flex-column mb-auto">
                 <li className="nav-item">
-                    <a href="#" className="nav-link active" aria-current="page" style={{color: darkMode ? '' : 'white-text'}} >
+                    <a href="#" className="nav-link active" aria-current="page" style={colorMode} >
                         Dashboard
                     </a>
                 </li>
                 <li>
-                    <a href="#" className="nav-link" style={{color: darkMode ? 'black' : '#f2f2f2'}} >
+                    <a href="#" className="nav-link" style={colorMode} >
                         Users
                     </a>
                 </li>
                 <li>
-                    <a href="#" className="nav-link" style={{color: darkMode ? 'black' : '#f2f2f2'}} >
+                    <a href="#" className="nav-link" style={colorMode} >
                         Orders
                     </a>
                 </li>
                 <li>
-                    <a href="#" className="nav-link" style={{color: darkMode ? 'black' : '#f2f2f2'}} >
+                    <a href="#" className="nav-link" style={colorMode} >
                         Products
                     </a>
                 </li>
                 <li>
-                    <a href="#" className="nav-link" style={{color: darkMode ? 'black' : '#f2f2f2'}} >
+                    <a href="#" className="nav-link" style={colorMode} >
                         Customers
                     </a>
                 </li>
