8474938bb3555dd765785d4400c472706397efcd
Muriithi Gakuru <antony123muriithi@gmail.com>
2022-10-09 16:22:27 +0300

    add nav n theme

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import NavBar from './components/NavBar';
+// import ToggleDark from './components/toggleDark';
+
 
 function App() {
   return (
     <div className="App">
-     <NavBar />
+      <NavBar />
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,43 @@
-import Container from 'react-bootstrap/Container';
+import React, { useState } from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { FaSun, FaMoon } from "react-icons/fa";
+import { Button, Container } from 'react-bootstrap';
+import { ThemeContext, themes } from '../contexts/ThemeContext';
 
 function NavBar() {
+    const [darkMode, setDarkMode] = useState(true);
     return (
-      <Navbar bg="light" expand="lg">
-        <Container>
-          <Navbar.Brand href="#home">Boy-Spyce</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mx-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">About</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">Boy-Spyce</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mx-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">About</Nav.Link>
+                        <Nav.Link href="#link">Contact</Nav.Link>
+                        <Nav.Link href="#link">FAQs</Nav.Link>
+                    </Nav>
+                    <ThemeContext.Consumer>
+                        {({ changeTheme }) => (
+                            <Button
+                                color="link"
+                                className='btn-warning ml-auto'
+                                onClick={() => {
+                                    setDarkMode(!darkMode);
+                                    changeTheme(darkMode ? themes.light : themes.dark);
+                                }}
+                            >
+                                <span>Switch mode {darkMode ? <FaSun /> : <FaMoon />}</span>
+                            </Button>
+                        )}
+                    </ThemeContext.Consumer>
+                </Navbar.Collapse>
+
+            </Container>
+        </Navbar>
     );
-  }
-  
-  export default NavBar;
+}
+
+export default NavBar;
