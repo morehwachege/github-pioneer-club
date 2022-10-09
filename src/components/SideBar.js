85e2033023167d31282ac2ec0dae420f5e70de20
Muriithi Gakuru <antony123muriithi@gmail.com>
2022-10-10 01:37:06 +0300

    add change

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -7,12 +7,18 @@ function SideBar({ darkMode }) {
         backgroundColor: darkMode ? '#f2f2f2' : 'black',
         color: darkMode ? 'black' : 'white'
     }
-    const colorMode = {color: darkMode ? 'black' : '#f2f2f2'}
+    const colorMode = { color: darkMode ? 'black' : '#f2f2f2' }
     return (
         <div className="d-flex flex-column flex-shrink-0 p-3 d-none d-md-block" style={styles}>
             <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
             </a>
             <ul className="nav nav-pills flex-column mb-auto">
+                <li>
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="here" aria-label="username" aria-describedby="basic-addon2" />
+                        <span class="input-group-text" id="basic-addon2">search</span>
+                    </div>
+                </li>
                 <li className="nav-item">
                     <a href="#" className="nav-link active" aria-current="page" style={colorMode} >
                         Dashboard
