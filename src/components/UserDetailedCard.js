ce7deda24b1be9dd072a369ac7ae68217158073a
Muriithi Gakuru <antony123muriithi@gmail.com>
2022-10-10 11:38:24 +0300

    add user organizations

--- src/components/UserDetailedCard.js
+++ src/components/UserDetailedCard.js
@@ -1,32 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import NavBar from './NavBar';
 import SideBar from './SideBar';
+import { FaGithub, FaFacebookF } from "react-icons/fa";
 
 function UserDetailedCard({ darkMode, setDarkMode, gitUser, setShowGitUser, showGitUser }) {
+    const [orgs, setOrgs] = useState([])
     let { id } = useParams();
     const style = {
         width: 100 + "%"
     }
-    
     const user = showGitUser.find(user => user.id === Number(id));
+    useEffect(() => {
+        fetch(`${user.organizations_url}`)
+            .then(res => res.json())
+            .then(data => {
+                setOrgs(data)
+            })
+    }, [])
+
     return (
         <>
             <NavBar darkMode={darkMode} setDarkMode={setDarkMode} />
             <div className='d-flex' style={style} >
                 <SideBar darkMode={darkMode} gitUser={gitUser} setShowGitUser={setShowGitUser} />
-                <div className='d-flex justify-content-center align-items-center flex-wrap' style={style}>
+                <div className='d-flex justify-content-center align-items-center flex-wrap flex-column' style={style}>
                     {
                         user ?
-                            <div className="card m-3" style={{ width: 24 + 'rem', background: darkMode ? 'white' : 'black' }}>
-                                <img className="card-img-top" src={user.avatar_url} alt={`${user.login}'s github avatar`} />
+                            <>
+                                <div className="card m-3" style={{ width: 24 + 'rem', background: darkMode ? 'white' : 'black' }}>
+                                    <img className="card-img-top" src={user.avatar_url} alt={`${user.login}'s github avatar`} />
 
-                                <div className="card-body d-flex justify-content-center align-items-center flex-column">
-                                    <h5 className="card-title">{user.login}</h5>
-                                    <p className="card-text">{user.name ? user.name : ''}</p>
-                                    <a href={user.html_url} className="btn btn-warning">See full profile</a>
+                                    <div className="card-body d-flex justify-content-center align-items-center flex-column">
+                                        <h5 className="card-title">{user.login}</h5>
+                                        <p className="card-text">{user.name ? user.name : ''}</p>
+                                        <a href={user.html_url} className="btn btn-warning">See full profile</a>
+                                    </div>
                                 </div>
-                            </div>
+                                <div className="card-body d-flex justify-content-center align-items-center flex-column">
+                                    <h2 className="card-title">Profiles</h2>
+                                    <a href={user.html_url}>
+                                        <p className='text-center d-flex justify-content-center align-items-center' style={{ fontSize: 20 + "px" }}>
+                                            <span className='m-3'>GitHub</span> <FaGithub /></p>
+                                    </a>
+                                    <h5 className="card-title">Username: {user.login}</h5>
+                                    <p className="card-text">Followers: {user.followers_url.length}</p>
+                                    <p className="card-text">Following: {user.following_url.length}</p>
+                                        <h3>Organizations</h3>
+                                    <div className='container d-flex justify-content-center align-items-center flex-row flex-wrap'>
+                                        {
+                                            // fetchOrgs(user.organisations_url)
+                                            orgs ?
+                                                orgs.map(item => {
+                                                    // console.log(item)
+                                                    return (
+                                                        <div key={item.id}>
+                                                            <img className='m-3'  height={100} width={100} src={item.avatar_url} />
+                                                            <p>{item.login}</p>
+                                                        </div>
+                                                    )
+
+                                                })
+                                                :
+                                                ""
+                                        }
+                                    </div>
+                                </div>
+                            </>
                             :
                             <p className='text-center'>Loading...</p>
                     }
